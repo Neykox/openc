99343cd299426240667a436119b93124f5c512a6
Neykox <anthonyleroy2017@gmail.com>
2019-01-24 20:34:07 +0100

    Initial Commit

---
+++ openc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  openc
+//
+//  Created by Family on 24/01/2019.
+//  Copyright © 2019 Family. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
